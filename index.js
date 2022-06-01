bd93f5b8b4f05af85ce3680d6ca4893d2f209f32
jackuson14 <jacksonlzc14@gmail.com>
2022-06-02 02:59:43 +0800

    [Update] Nothing Generation V2

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 const fs = require('fs')
 const { createCanvas } = require('canvas')
 
+const totalSupply = 5666
 const width = 1200  
 const height = 1200
-const opacity = Math.random() * (0.8 - 0.3) + 0.3
-const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-const canvas = createCanvas(width, height)
-const context = canvas.getContext('2d')
+function generate(){
+    const opacity = Math.random() * (0.8 - 0.3) + 0.3
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
-context.fillStyle = '#000'
-context.fillRect(0, 0, width, height)
-
-const text = 'NOTHING.'
-
-context.font = '50pt Sans'
-context.textAlign = 'center'
-context.fillStyle = "#" + randomColor
-context.fillText(text, Math.floor(Math.random() * (1000 - 400) + 400), Math.floor(Math.random() * (1000 - 400) + 400))
-
-for ( x = 0; x < canvas.width; x++ ) {
-    for ( y = 0; y < canvas.height; y++ ) {
-       number = Math.floor( Math.random() * 255 );
-
-       context.fillStyle = "rgba(" + number + "," + number + "," + number + "," + opacity + ")";
-       context.fillRect(x, y, Math.random() * 2, Math.random() * 2);
+    const canvas = createCanvas(width, height)
+    const context = canvas.getContext('2d')
+    
+    context.fillStyle = '#000'
+    context.fillRect(0, 0, width, height)
+    
+    const text = 'NOTHING.'
+    
+    context.font = '50pt Sans'
+    context.textAlign = 'center'
+    context.fillStyle = "#" + randomColor
+    context.fillText(text, Math.floor(Math.random() * (1000 - 400) + 400), Math.floor(Math.random() * (1000 - 400) + 400))
+    
+    for ( x = 0; x < canvas.width; x++ ) {
+        for ( y = 0; y < canvas.height; y++ ) {
+           number = Math.floor( Math.random() * 200 );
+    
+           context.fillStyle = "rgba(" + number + "," + number + "," + number + "," + opacity + ")";
+           context.fillRect(x, y, Math.random() * 2, Math.random() * 2);
+        }
     }
- }
+    return canvas
+}
 
-const buffer = canvas.toBuffer('image/png')
-fs.writeFileSync('./image.png', buffer)
+for( z = 0; z < totalSupply; z++ ){
+    const canva = generate()
+    const buffer = canva.toBuffer('image/png')
+    fs.writeFileSync(`./generation/image/#${z}.png`, buffer)
+}
